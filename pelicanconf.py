03050e66118721562ee2d2bb2cfbfddeb482424a
Ryan Lerch <rlerch@redhat.com>
2021-04-14 14:01:20 +1000

    Add initial proof of concept

--- pelicanconf.py
+++ pelicanconf.py
@@ -7,28 +7,43 @@ SITEURL = ''
 
 PATH = 'content'
 
+THEME = 'theme'
+
+USE_FOLDER_AS_CATEGORY = True
+
+AUTHORS_SAVE_AS = ''
+AUTHOR_SAVE_AS = ''
+TAG_SAVE_AS = ''
+TAGS_SAVE_AS = ''
+CATEGORY_SAVE_AS = ''
+CATEGORIES_SAVE_AS = ''
+
 TIMEZONE = 'UTC'
 
 DEFAULT_LANG = 'en'
 
+#Use basename(filename) rather than title for slugs 
+SLUGIFY_SOURCE = 'basename'
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
+CATEGORY_FEED_RSS = 'feeds/{slug}.rss'
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
-
 DEFAULT_PAGINATION = False
+
+PLUGIN_PATHS = ['plugins']
+PLUGINS = ['filters']
+
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+DIRECT_TEMPLATES = (('index', 'resolved'))
+
+# Some specific settings just for status.fp.o
+
+TICKET_TRACKER_URL = 'https://pagure.io/fedora-infrastructure/issue/'
